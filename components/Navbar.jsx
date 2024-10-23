008eb85ace612acf51d54e53ea624fca81a11648
Ali28202 <mahallati81@gmail.com>
2024-10-23 14:55:33 -0700

    Navbar Section Complete (Mobile Design need sheet)

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+export default function Navbar() {
+	return (
+		<>
+			<DesktopNavbar />
+			<MobileNavbar />
+		</>
+	);
+}
